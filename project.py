0b255e4fd2fbe19a1c402dae7a616223c1a8c707
KondoA9 <kondo.eque@gmail.com>
2020-11-06 22:51:56 +0900

    initial commit

---
+++ project.py
@@ -0,0 +1,50 @@
+from tensorflow.keras import callbacks, utils
+
+import os
+
+
+class Project:
+
+    callbacks = [None] * 0
+
+    def __init__(self, name, tb_update_freq='batch', lr_setter=None):
+        self.name = name
+
+        # directories
+        self._tensorboard_dir = "../tmp/tb/" + name + '/'
+        self._checkpoint_dir = "../tmp/cp/" + name + '/'
+        self._checkpoint_path = self._checkpoint_dir + 'checkpoint'
+
+        # callbacks
+        self.callbacks.append(callbacks.ModelCheckpoint(
+            self._checkpoint_path, save_weights_only=True, verbose=1))
+
+        self.callbacks.append(callbacks.TensorBoard(
+            log_dir=self._tensorboard_dir, histogram_freq=1,
+            update_freq=tb_update_freq))
+
+        if not lr_setter == None:
+            self.callbacks.append(callbacks.LearningRateScheduler(
+                lr_setter,
+                verbose=0))
+
+        # create directories
+        os.makedirs(self._checkpoint_dir, exist_ok=True)
+
+    def _exist_ckpt(self):
+        return os.path.exists(self._checkpoint_path)
+
+    def plot_model(self, model):
+        utils.plot_model(model,
+                         '../model_image/' + self.name + '.png',
+                         show_shapes=True)
+
+
+    def load_ckpt(self, model):
+        if self._exist_ckpt():
+            print("Load checkpoint: " + self._checkpoint_path)
+            model.load_weights(self._checkpoint_path)
+        else:
+            print("Checkpoint is not found: " + self._checkpoint_path)
+
+        return model
