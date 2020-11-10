8836215ecb56a0e1849080e953620e1c2bbe1de3
KondoA9 <kondo.eque@gmail.com>
2020-11-10 15:34:38 +0900

    edit resnet

--- models/resnet.py
+++ models/resnet.py
@@ -20,15 +20,60 @@ def _ResidualBlock(input, filters, kernels, downsample=False, final_block=False)
 
     return output
 
+def _ResNetLayer(input, filter_kernel_shape_list, num, final_block=False):
+    x = None
+
+    l_num = len(filter_kernel_shape_list)
+
+    for i, l in enumerate(filter_kernel_shape_list):
+        for j in range(num):
+            if i == 0 and j == 0:
+                x = _ResidualBlock(input, l[0], l[1], downsample=True, final_block=final_block)
+            elif i == l_num - 1 and j == num - 1:
+                x = _ResidualBlock(x, l[0], l[1], downsample=True, final_block=final_block)
+            else:
+                x = _ResidualBlock(x, l[0], l[1], downsample=True, final_block=final_block)
+
+    return x
+
 class ResNet:
     
     @staticmethod
     def build(input_shape, label_num):
         #first layer
         inputs_layer = layers.Input(shape=input_shape)
-        output = layers.Conv2D(64, 3)(inputs_layer)
-        output = layers.MaxPool2D()(output)
+        output = layers.Conv2D(64, 7, strides=2)(inputs_layer)
+        output = layers.MaxPool2D(pool_size=(3, 3), strides=2)(output)
 
+        l = [
+            (64, 1),
+            (64, 3),
+            (256, 1)
+        ]
+        x = _ResNetLayer(output, l, 3)
+
+        l = [
+            (128, 1),
+            (128, 3),
+            (512, 1)
+        ]
+        x = _ResNetLayer(output, l, 4)
+
+        l = [
+            (256, 1),
+            (256, 3),
+            (1024, 1)
+        ]
+        x = _ResNetLayer(output, l, 6)
+
+        l = [
+            (512, 1),
+            (512, 3),
+            (2048, 1)
+        ]
+        x = _ResNetLayer(output, l, 3, final_block=True)
+
+        '''
         #conv_1
         x = _ResidualBlock(output, 64, 3)
         x = _ResidualBlock(x, 64, 3)
@@ -48,6 +93,7 @@ class ResNet:
         x = _ResidualBlock(x, 512, 3, True)
         x = _ResidualBlock(x, 512, 3)
         x = _ResidualBlock(x, 512, 3, final_block=True)
+        '''
 
         #dense
         x = layers.GlobalAveragePooling2D()(x)
